3b5e87103d5fa49de3487aeb8644b43c2db71202
rbutus <rbutustree@gmail.com>
2018-11-02 14:24:55 -0700

    Modification to the test file, added .DS_Store and .idea to ~/.gitignore_global file

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -6,8 +6,11 @@ import excon
 from excon import convert
 from excon.convert import pdf2csv
 
-def test_input():
-    assert
+
+def test_input(monkeypatch):
+    monkeypatch.setattr('builtins.input', lambda x: "y")
+
+
 
 
 
