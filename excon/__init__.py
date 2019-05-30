45a99e8faa21a3c0275018cf9592b49c47bb5db2
rbutus <rbutustree@gmail.com>
2019-05-30 12:32:33 -0700

    fixed a typo in __init__.py

--- excon/__init__.py
+++ excon/__init__.py
@@ -1,6 +1,6 @@
 from .convert import pdf2csv
 from .convert import csv2exc
-from .convert import conver_pdf
+from .convert import convert_pdf
 from .convert import ocr
 from .convert import wrangle
 from .convert import extract_pages
